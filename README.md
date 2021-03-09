c92722ff9bca085ddba851e5c8fef219e716499b
Siri Nilsen <sirinilsen95@gmail.com>
2021-03-09 16:20:47 +0100

    forsøk nr 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # oppgave1og2
+Her prøver jeg å lage en README.md som beskriver repository
---
+++ SiriNilsen.java
@@ -0,0 +1,6 @@
+class SiriNilsen {
+
+	public static void main(String args[]){
+		System.out.println("Siri Nilsen");
+	}
+}
